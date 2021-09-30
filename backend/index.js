86369fb9d43d49e903028b74d65806527d8b22ce
Faruk Sinanovic <fsinanovic1@gmail.com>
2021-09-30 12:44:57 +0200

    added initial page and endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 const PORT = 3000;
 
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/", (req, res) => {
+  res.send({
+    done: true,
+  });
+});
+
+app.post("/doMagic", (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(PORT, () => {
