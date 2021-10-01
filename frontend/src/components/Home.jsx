87f278bb9909a59c9de51bb4f8265f227b4a0df2
Faruk Sinanovic <fsinanovic1@gmail.com>
2021-10-01 15:26:01 +0200

    done some stuff

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,7 +5,29 @@ import {AppBar, Grid, IconButton, Toolbar, Typography} from "@mui/material";
 import "./home.css";
 import sprintingLogo from '../assets/logo.png';
 import image from '../assets/image.png';
+import {Messages, MESSAGES} from './Messages';
+
 export const Home = () => {
+    const [messageState, setMessageState] = React.useState('installing');
+    const timeoutRef = React.useRef(0);
+
+    React.useEffect(() => {
+        return;
+        if(messageState){
+            const currentIndex = MESSAGES.findIndex(message => message.id === messageState);
+            const currentMessage = MESSAGES[currentIndex] ?? MESSAGES[0];
+            const nextMessage = MESSAGES[currentIndex + 1] ?? MESSAGES[0];
+            console.log("EVO MEEEEE", currentMessage);
+            if(currentMessage.id === 'hidden'){
+                return;
+            }
+            setTimeout(() => {
+                setMessageState(nextMessage.id);
+            }, currentMessage.timeout);
+        }
+    }, [messageState])
+    
+
     return (
         <>
             <AppBar position="static" className={"header"} elevation={3}>
@@ -33,14 +55,21 @@ export const Home = () => {
                         <TextField label="Please enter your github repository" color={"secondary"} className={"textField"} focused />
                 </Grid>
                 <Grid item xs={12}>
-                        <Button variant="contained">Let the magic happen!</Button>
+                        <Button onClick={() => {
+                        setMessageState('cloning');
+                    }} variant="contained">Let the magic happen!</Button>
                 </Grid>
                 <Grid item xs={2} >
                 </Grid>
-                <Grid item xs={4} >
+                {/* <Grid item xs={4} >
                     <Typography variant="h4" component="div" gutterBottom className={"text"}>
                         TURN YOUR HIGH QUALITY DEVOPS INTO USEFUL BUTTON
                     </Typography>
+                </Grid> */}
+                {/* <img src={'https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Finstall.appcenter.ms%2Fusers%2Fmilan.gluhic%2Fapps%2Fdemoapp%2Fdistribution_groups%2Fpublic&chs=180x180&choe=UTF-8&chld=L%7C2'} rel='nofollow' alt='qr code'></img> */}
+
+                <Grid item xs={4}>
+                    <Messages messageState={messageState} />
                 </Grid>
                 <Grid item xs={6} >
                     <img className={"image"} src={image}/>
---
+++ frontend/src/components/Messages.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Typography} from "@mui/material";
+import "./messages.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
+
+export const MESSAGES = [
+  {  timeout: 4000,     message: 'Preparing the git repository', id: 'cloning'},
+  {  timeout: 6000,     message: 'Analysing your app', id: 'analysing'},
+  {  timeout: 4000,     message: 'Moving build configurations to the project', id: 'moving'},
+  {  timeout: 8000,     message: 'Installing necessary dependencies', id: 'installing'},
+  {  timeout: 12000,    message: 'Creating the build pipeline', id: 'creating'},
+  {  timeout: 1000,     message: 'Automatically generating build pipelines', id: 'pipeline'},
+  {  timeout: 3000,     message: 'Generating Android signing keystore', id: 'keystore'},
+  {  timeout: 13000,    message: 'Building the Android app', id: 'building'},
+  {  timeout: 8000,     message: 'Pulling iOS provisioning profiles', id: 'provisioning'},
+  {  timeout: 6000,     message: 'Attaching iOS provisioning profiles to the XCode project', id: 'xcode'},
+  {  timeout: 12000,    message: 'Building the iOS app', id: 'buildingios'},
+  {  timeout: 13000,    message: 'Pushing the built apps to Microsoft AppCenter', id: 'appcenter'},
+  {  timeout: 100,     message: 'All done!', id: 'done'},
+  {  timeout: 0,        message: '' ,id: 'hidden'},
+]
+export const Messages = ({messageState}) => {
+  const messageStateIndex = MESSAGES.findIndex(message => message.id === messageState);
+
+  return (
+    <div>
+      {
+        MESSAGES.map((data, index) => (
+          <div style={{display: 'flex'}}>
+            {
+              messageStateIndex === index && <Loader type="Triangle" color="#00BFFF" height={40} width={40} />
+            }
+          <Typography id={data.id} variant="h10" component="div" gutterBottom className={`message ${messageStateIndex < index ? 'gray': ''}`} color={messageStateIndex === index ? 'primary': 'secondary'}>
+              {data.message}
+          </Typography>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
